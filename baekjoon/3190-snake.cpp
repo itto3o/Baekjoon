99bb701b7c920057b4a1c48290c13c43f1a70266
itto3o <208ehd@naver.com>
2023-05-16 21:27:29 +0900

    snake partially update

--- baekjoon/3190-snake.cpp
+++ baekjoon/3190-snake.cpp
@@ -89,13 +89,30 @@
 * 벽은 내부적으로는 없다.
 * 방향을 트는 시간이 1초이진 않는것같다.
 * 방향을 트는 시간은 따로 없고, 시작 블록에서 1초를 잡진 않는 것 같다.
+* 8초까지 지난 후 9초에 닿이면 9초가 답이다.
 * 
 * 약간 문제를 잘 읽어보면 답이 있는 것 같기도하다
+* 
+* 처음엔 오른쪽 위에서 시작, 오른쪽으로 가야함
+* 지정된 시간까지(3 D면 4초 시작부터 방향 틈)
+* 1초가 시작되면 뱀은 머리를 먼저 옮기고, 꼬리를 옮김
+* 머리가 사과에 닿이면 꼬리는 옮기지 않음(사과는 없어짐)
+* 머리가 벽이나 자기 몸에 닿이면 게임이 끝남(그 시간을 return)
+* 
+* ==> 처음 방향은 오른쪾으로 해두고
+* map에 사과 위치를 넣을까 했는데 그냥 구조체 x,y구해서 배열로 하고 있네
+* 정해진 방향으로 머리를 먼저 움직이고, 그때 사과랑 닿았다면 꼬리는 안움직이기, 
+* 아 근데 그냥 칸을 먹는 방식으로 할까... board에 뱀이 있는 곳을 표시하는거지
+* 그러면 꼬리에 닿으면 바로 알거 아냐
+* board에 1로 만들면서 지나온곳은 0으로 만들고, 근데 그럼 꼬리 움직이고 말고?를 특정짓기 힘들려나
+* 으악 모르겠따 머리랑 꼬리 부분 좌표가 필요 없을까?
 * 
 */
 
 #include <iostream>
 #include <vector>
+#include <map>
+
 using namespace std;
 
 enum {
@@ -111,13 +128,28 @@ struct Position
 class Snake
 {
 public:
-	int snakeLen = 1;
+	int snakeLen;
 	Position headPosition;
 	Position tailPosition;
 	vector<Position> chargePosition;
-	int goingDirection = RIGHT;
+	int goingDirection;
 
-	void go()
+	Snake()
+	{
+		snakeLen = 1;
+		headPosition.x = 0;
+		headPosition.y = 0;
+		tailPosition.x = 0;
+		tailPosition.y = 0;
+		goingDirection = RIGHT;
+	}
+
+	void GoHead()
+	{
+
+	}
+
+	void GoTail()
 	{
 
 	}
@@ -125,6 +157,8 @@ public:
 
 int main()
 {
+	int answerTime = 0;
+	Snake snake;
 	int N = 0;
 	int K = 0;
 
@@ -136,7 +170,6 @@ int main()
 	for (int i = 0; i < N; i++)
 		board[i] = new int[N];
 
-	// 초기화(시켜야 되나?) 애초에 필요가 있나?
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < N; j++)
@@ -144,6 +177,8 @@ int main()
 			board[i][j] = 0;
 		}
 	}
+	// 뱀 시작점은 1로 만들기
+	board[0][0] = 1;
 
 	cin >> K;
 	if (K < 0 || K > 100)
@@ -159,19 +194,104 @@ int main()
 	if (L < 1 || L > 100)
 		return -1;
 
-	vector<int, char> transformDirection;
+	map<int, char> transformDirection;
 	int time = 0;
 	char direction = 'D';
 	for (int i = 0; i < L; i++)
 	{
 		cin >> time >> direction;
-		transformDirection.push_back(time);//, direction }); //잉 이렇게 하는 게 맞나
-		// 아몰랑 오늘은 여기까지만 할래...
+		transformDirection.insert({ time, direction }); //, direction }); //잉 이렇게 하는 게 맞나 (그래ㅓㅅ map으로 함)
 	}
+
+	// 정해진 방향으로 한 칸 움직이는데
+	// 이때 앞 칸에 뭔가 있는지 확인
+	// 움직이기 전 time+1
+	while (true)
+	{
+		Position currentHead = snake.headPosition;
+
+		Position forwardHead;
+		// head가 앞으로 갈 칸
+		switch (snake.goingDirection)
+		{
+		case UP :
+			forwardHead = { currentHead.y - 1, currentHead.x };
+			break;
+		case DOWN:
+			forwardHead = { currentHead.y + 1, currentHead.x };
+			break;
+
+		case LEFT :
+			forwardHead = { currentHead.y, currentHead.x - 1 };
+			break;
+
+		case RIGHT:
+			forwardHead = { currentHead.y, currentHead.x + 1 };
+			break;
+
+		default:
+			break;
+		}
+
+		// 만약 앞으로 갈 칸이 벽이거나, 자기 자신이면 게임끝
+		if (forwardHead.x < 0 || forwardHead.x >= N || forwardHead.y < 0 || forwardHead.y >= N)
+		{
+			answerTime++;
+			break;
+		}
+
+		if (board[forwardHead.y][forwardHead.x] == 1)
+		{
+			answerTime++;
+			break;
+		}
+
+		// 만약 둘 다 아니라면 일단 시간은 +1
+		answerTime++;
+
+		// 이제 옮겨야 하는데
+		// 만약 사과가 있다면 GoHead만, board를 1로 채우기
+		for (int i = 0; i < K; i++)
+		{
+			if (forwardHead.y == apples[i].y && forwardHead.x == apples[i].x)
+			{
+				// 사과가 있는 곳이라면 GoHead()
+				snake.GoHead();
+				board[forwardHead.y][forwardHead.x] == 1;
+			}
+		}
+
+		// 사과가 없었다면 GoHead, GoTail, board 한칸 미루기
+		// 근데 만약 ㄱ자로 뱀이 있었다면 머리랑 tail만 옮긴다고 될 일은 아닌데
+		// board를 칠하는 부분을 따로 만들어야하나..? 으악 좀 쉴까.......
+
+		// 만약 지금 시간(바꾸고 난 후 시간)이 방향을 전환해야하는 시간(이 시간이 끝난 후 바꿔야 함)이라면
+		for (auto td : transformDirection)
+		{
+			if (td.first == answerTime)
+			{
+				// 방향을 바꿔줌
+				// 현재 방향에 +1을 하면 오른쪽, -1을 하면 왼쪽
+				if (td.second == 'D')
+				{
+					snake.goingDirection = (snake.goingDirection + 1) % 4; // 순환 구조로 만약 left(3)에서 +1을 하면 0이 나오도록
+				}
+				else if (td.second == 'L')
+				{
+					snake.goingDirection = (snake.goingDirection + 3) % 4; // 순환 구조로 만약 up(0)에서 -1을 하면 3이 나오도록
+				}
+			}
+		}
+
+
+	}
+
+	// 결과 출력
+	cout << answerTime << endl;
 	
 	for (int n = 0; n < N; n++)
 		delete[] board[n];
 	delete[] board;
-	return 0;
+
 	return 0;
 }
